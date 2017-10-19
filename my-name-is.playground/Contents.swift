bde07d346aea8bd2e76aba907d581a7d17e88f97
Paul Gross <pgross@maryville.edu>
2017-10-19 01:11:24 -0500

    Initial commit

---
+++ my-name-is.playground/Contents.swift
@@ -0,0 +1,5 @@
+// Replace the YOUR_NAME between the quotation marks below with your name.
+// You should see it print out in the console at the bottom of this window
+var str = "YOUR_NAME"
+
+print( "Hello, my name is", str )
